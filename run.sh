e2e45691dbdea6cc9bf23d1eecdf2e30207d93e7
Frank Zickert <webmaster@clued-up.de>
2019-07-23 13:10:32 +0200

    updated run-script, bugfix

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t jpl_config -f config/{§identifier§}.Dockerfile .
+docker build -t {§identifier§} -f config/{§identifier§}.Dockerfile .
 docker run -ti -v ${PWD}:/usr/local/bin/{§identifier§} -p {§port§}:8888 {§identifier§}
